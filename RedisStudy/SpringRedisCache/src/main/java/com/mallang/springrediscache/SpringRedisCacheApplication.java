c4aecbc4d4af2a588d1ddbd33bc60edb409f4b77
sunyeon-Jeong <sunyeon4012@gmail.com>
2025-03-08 14:31:13 +0900

    Feat : RedisStudy - UserRepository 생성 - application.java -> UserDB data insert logic

--- RedisStudy/SpringRedisCache/src/main/java/com/mallang/springrediscache/SpringRedisCacheApplication.java
+++ RedisStudy/SpringRedisCache/src/main/java/com/mallang/springrediscache/SpringRedisCacheApplication.java
@@ -1,13 +1,33 @@
 package com.mallang.springrediscache;
 
+import com.mallang.springrediscache.domain.entity.User;
+import com.mallang.springrediscache.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class SpringRedisCacheApplication {
+@EnableJpaAuditing
+@RequiredArgsConstructor
+public class SpringRedisCacheApplication implements ApplicationRunner {
+
+    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRedisCacheApplication.class, args);
+    }
+
+    // ApplicationRunner : 애플리케이션 시작직후 실행되는 코드블럭
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        userRepository.save(User.builder().name("mallang").email("mallang@mallang.com").build());
+        userRepository.save(User.builder().name("chunsik").email("chunsik@chunsik.com").build());
+        userRepository.save(User.builder().name("grommit").email("grommit@grommit.com").build());
+
     }
 
 }
